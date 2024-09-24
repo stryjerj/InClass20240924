b24adb6fa17749dfcca944f76b55539f8b6a6e38
stryjerj <rstryjewski10@gmail.com>
2024-09-24 08:57:51 -0400

    Made changes during class

--- InClass20240924/functionPackage/dictionaries.py
+++ InClass20240924/functionPackage/dictionaries.py
@@ -1,1 +1,63 @@
 # dictionaries.py
+
+
+def demo():
+    """
+    Demonstate basic dictionary stuff
+    """
+    myDictionary = {"Cincinnati":"Bearcats", "Xavier":"Musketeers", "NKU":"Norse", "UC Clermont":"Cougars"}
+    print(myDictionary)
+    
+
+    # Iterate over the dictionary by ket
+    
+    for key in myDictionary.keys():
+        print(key)
+        
+
+    # Iterate by value
+    
+    for item in myDictionary.items():
+        print(item)
+        
+    # Iterate by value
+    
+    for value in myDictionary.values():
+        print(value)
+
+    # Add a key/value pair to the dictionary
+    
+    myDictionary["Michigan State"] = "Spartans"
+    
+    print(len(myDictionary))
+    
+    # Cause an exception. Add try / except logic to handle this
+    
+    try:
+        print(myDictionary["Ohio State"])
+    except:
+        # We end up here if exception is thrown in the try block
+        print("Ohio State is a valid key")  
+    
+
+    # Remove Xavier from the list
+    
+    myDictionary.pop("Xavier")
+    print(myDictionary)
+    
+    # Create another dictionary called newTeams
+    # Add several key/value pairs
+    #combine that with myDictionary and print the results
+    
+    newTeams = {"Ohio State":"Buckeyes", "Michigan":"Wolverines", "Indiana":"Hoosiers", "Purdue":"Boilermakers"}
+    """
+    # Brute Force
+    
+    for key in newTeams.keys():
+        myDictionary[key] = newTeams[key]
+    print(myDictionary)
+    """
+    
+
+    myDictionary.update(newTeams)
+    print(myDictionary)
--- InClass20240924/mainPackage/main.py
+++ InClass20240924/mainPackage/main.py
@@ -1,2 +1,9 @@
 # main.py
 
+from functionPackage.dictionaries import demo
+
+
+if __name__ == "__main__":
+    print("I am main.py")
+    demo()
+
